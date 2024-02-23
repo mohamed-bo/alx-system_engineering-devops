8f2b83c35f95bf25ead6163161fd3298f5f8f6d4
mohamed-bo <medprgrm@gmail.com>
2024-02-23 15:23:15 +0100

    inchalah it works

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,21 @@
 # Install flask from pip3
 
-package { 'pip3':
+class { 'python':
+  ensure => 'present',
+}
+
+package { 'python3-pip':
   ensure => 'installed',
 }
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  require => [Class['python'], Package['python3-pip']],
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug',
+  path    => '/usr/bin',
+  require => [Class['python'], Package['python3-pip']],
 }
