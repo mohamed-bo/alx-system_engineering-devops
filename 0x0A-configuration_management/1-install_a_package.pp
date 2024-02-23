dc6bc5b56dc03e03b77ecc1402c2630c15365654
mohamed-bo <medprgrm@gmail.com>
2024-02-23 15:09:23 +0100

    inchalah it works

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,10 @@
 # Install flask from pip
 
-class { 'python':
-  ensure => 'present',
-}
-
-package { 'python3-pip':
+package { 'pip':
   ensure => 'installed',
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin',
-  require => [Class['python'], Package['python3-pip']],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
