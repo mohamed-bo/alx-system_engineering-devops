332108bf012b6fe22af37a3acc202fc9f00fae20
mohamed-bo <medprgrm@gmail.com>
2024-02-26 22:14:44 +0100

    inchalah it works

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+#install nginx
+
+package {
+    'nginx':
+    ensure => installed,
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
+}
+
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\t github ^/redirect_me https://github.com/mohamed-bo",
+}
+
+service {'nginx':
+    ensure => running,
+}
