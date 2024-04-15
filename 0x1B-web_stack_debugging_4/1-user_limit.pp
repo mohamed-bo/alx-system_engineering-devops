b9831c13c29ffc31988fccd75f047349170d2472
mohamed-bo <medprgrm@gmail.com>
2024-04-15 18:51:45 +0100

    inchalah it works

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Web stack debugging #4
+
+exec { 'increase-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
+}
+
+#Increase soft limit
+
+exec { 'increase-soft-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
+}
