37dc8b7d71b63cf62e6abeedb826876ca8b44f3d
mohamed-bo <medprgrm@gmail.com>
2024-06-04 20:47:30 +0100

    inchalah it works

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,15 @@ import requests
 def number_of_subscribers(subreddit):
     """the Reddit API returns the number of subscribers"""
     import requests
-
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
-    reddi = requests.get("https://www.reddit.com/r/{}/about.json"
-                         .format(subreddit),
-                         headers=headers,
-                         allow_redirects=False)
-    if reddi.status_code >= 300:
+    try:
+        headers = requests.utils.default_headers()
+        headers.update({'User-Agent': 'My User Agent 1.0'})
+        reddi = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers=headers,
+                            allow_redirects=False)
+        if reddi.status_code >= 300:
+            return 0
+        return reddi.json().get("data").get("subscribers")
+    except requests.RequestException:
         return 0
-    return reddi.json().get("data").get("subscribers")
